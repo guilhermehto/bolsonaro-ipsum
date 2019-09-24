2a873ae5d6ef26a15a822e3b7f8c0e31bfbacf1b
Guilherme Oliveira <htoguilherme@gmail.com>
2019-09-24 20:13:46 +1200

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,16 @@ const App: React.FC = () => {
         <hr/>
         <hr/>
         <div className="paper-info">
-          <p>10/07/1995</p>
+          <p>{new Date().toLocaleDateString("en-NZ")}</p>
           <p>Brasília - DF</p>
           <p>Brasil acima de todos</p>
         </div>
         <hr/>
         <hr/>
       </header>
+      <div className="hero-news">
+        <h1><span>TEM</span> <span>QUE</span> <span>MUDAR</span> <span>ISSO</span> <span>AÍ!</span></h1>
+      </div>
     </div>
   );
 }
