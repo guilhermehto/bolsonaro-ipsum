c27a030129376301cecdd89558fdef9c07e1c056
Guilherme Oliveira <htoguilherme@gmail.com>
2019-09-25 14:31:22 +1200

    Added text area component

---
+++ src/components/textarea/Textarea.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Textarea.scss';
+
+const Textarea = () => {
+    return (
+      <textarea className="textarea">
+      </textarea>
+    );
+  }
+  
+  export default Textarea;
