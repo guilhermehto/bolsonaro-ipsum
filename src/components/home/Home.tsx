d0cb91a04735f5e3f366bc212e37eaa0745a267c
Guilherme Oliveira <htoguilherme@gmail.com>
2019-09-26 16:55:08 +1200

    Added statement to home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.scss';
+import bolsonaro from './bolsonaro.png';
 
 import Input from '../input/Input';
 import Textarea from '../textarea/Textarea';
@@ -8,20 +9,30 @@ import Button from '../button/Button';
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <div className="form col">
-        <p>Nosso presidente acabou de fazer uma declaração com</p>
-        <Input type="number" />
-        <p>parágrafos.</p>
+      <div className="col">
+        <div className="form col">
+          <p>Nosso presidente acabou de fazer uma declaração com</p>
+          <Input type="number" />
+          <p>parágrafos.</p>
+        </div>
+        <div className="result col">
+          <span className="first-quote">"</span>
+          <Textarea />
+          <span className="second-quote">"</span>
+        </div>
+        <div className="buttons col">
+          <Button>GERAR</Button>
+          <div className="spacer"></div>
+          <Button>COPIAR</Button>
+        </div>
       </div>
-      <div className="result col">
-        <span className="first-quote">"</span>
-        <Textarea />
-        <span className="second-quote">"</span>
-      </div>
-      <div className="buttons col">
-        <Button>GERAR</Button>
-        <div className="spacer"></div>
-        <Button>COPIAR</Button>
+      <div className="col">
+        <div className="col bolsonaro-picture">
+          <img src={bolsonaro} alt="Presidente bolsonaro fazendo sinais de arminhas com as mãos e rindo pakas" />
+        </div>
+        <div className="col bolsonaro-statement">
+          <h1>"Olha, eu não falei isso aí não, tá okay?!"</h1>
+        </div>
       </div>
     </div>
   );
