f314e17df4b8f1afd8c1f1babb8c8ffa14d5bba9
Guilherme Oliveira <htoguilherme@gmail.com>
2019-09-27 16:23:11 +1200

    Added firestore and phrases service

---
+++ src/services/PhrasesService.ts
@@ -0,0 +1,39 @@
+import firebaseConfig from '../FirebaseConfig';
+
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+firebase.initializeApp(
+  firebaseConfig
+);
+
+const db = firebase.firestore();
+
+class PhrasesService {
+  private minPhrases = 3;
+  private maxPhrases = 6;
+
+  getPhrases = async (paragraphs: number = 1) => {
+    let statement = "";
+    const phrases : any = []
+    
+    const snapshot = await db.collection("phrases").get();
+    snapshot.forEach((doc) => {
+      phrases.push(doc.data());
+    });
+
+    for(let i = 0; i < paragraphs; i++) {
+      const phrasesCount = Math.floor(Math.random() * (this.maxPhrases - this.minPhrases + 1) + this.minPhrases);
+      for(let p = 0; p < phrasesCount; p++) {
+        const index = Math.floor(Math.random() * Math.floor(phrases.length));
+        statement += `${phrases[index].phrase} `;
+      }
+      statement += '\n\n';
+    }
+
+    return statement;
+  }
+
+}
+
+export default PhrasesService;
