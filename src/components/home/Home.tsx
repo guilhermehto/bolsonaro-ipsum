efd5ab1f1adfd12077e1766cc137134f2b0ef61f
Guilherme Oliveira <htoguilherme@gmail.com>
2019-09-27 17:06:29 +1200

    Added default values for lorem form

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,41 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.scss';
 import bolsonaro from './bolsonaro.png';
+
+import PhrasesService from '../../services/PhrasesService';
 
 import Input from '../input/Input';
 import Textarea from '../textarea/Textarea';
 import Button from '../button/Button';
 
-const Home: React.FC = () => {
-  return (
-    <div className="home">
-      <div className="col">
-        <div className="form col">
-          <p>Nosso presidente acabou de fazer uma declaração com</p>
-          <Input type="number" />
-          <p>parágrafos.</p>
+interface IHomeState {
+  paragraphsCount: number,
+  phrases: String,
+}
+
+export class Home extends Component<{}, IHomeState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      paragraphsCount: 1,
+      phrases: "",
+    }
+  }
+
+  phrasesService = new PhrasesService();
+
+  async componentWillMount() {
+    this.setState({ phrases: await this.phrasesService.getPhrases(this.state.paragraphsCount) })
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="col">
+          <div className="form col">
+            <p>Nosso presidente acabou de fazer uma declaração com</p>
+            <Input type="number" value={this.state.paragraphsCount}/>
+            <p>parágrafos.</p>
+          </div>
+          <div className="result col">
+            <span className="first-quote">"</span>
+            <Textarea value={this.state.phrases} readOnly />
+            <span className="second-quote">"</span>
+          </div>
+          <div className="buttons col">
+            <Button>GERAR</Button>
+            <div className="spacer"></div>
+            <Button>COPIAR</Button>
+          </div>
         </div>
-        <div className="result col">
-          <span className="first-quote">"</span>
-          <Textarea />
-          <span className="second-quote">"</span>
-        </div>
-        <div className="buttons col">
-          <Button>GERAR</Button>
-          <div className="spacer"></div>
-          <Button>COPIAR</Button>
+        <div className="col">
+          <div className="col bolsonaro-picture">
+            <img src={bolsonaro} alt="Presidente bolsonaro fazendo sinais de arminhas com as mãos e rindo pakas" />
+          </div>
+          <div className="col bolsonaro-statement">
+            <h1>"Olha, eu não falei isso aí não, tá okay?!"</h1>
+          </div>
         </div>
       </div>
-      <div className="col">
-        <div className="col bolsonaro-picture">
-          <img src={bolsonaro} alt="Presidente bolsonaro fazendo sinais de arminhas com as mãos e rindo pakas" />
-        </div>
-        <div className="col bolsonaro-statement">
-          <h1>"Olha, eu não falei isso aí não, tá okay?!"</h1>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
